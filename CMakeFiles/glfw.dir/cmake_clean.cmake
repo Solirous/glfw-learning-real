a12c21560cf6655335e935077471e8c00a80ace8
Solirous <99412371+Solirous@users.noreply.github.com>
2023-02-21 20:06:56 +0700

    hi

---
+++ CMakeFiles/glfw.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/glfw.dir/src/main.cpp.obj"
+  "CMakeFiles/glfw.dir/src/main.cpp.obj.d"
+  "glfw.exe"
+  "glfw.exe.manifest"
+  "glfw.pdb"
+  "libglfw.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/glfw.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
